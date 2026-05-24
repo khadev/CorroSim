4b655c058239c49057dc65bb0c45bc11f8260395
khadev <oukil.khaled@gmail.com>
2026-05-23 17:39:34 -0700

    Fix setup.py - embed requirements directly

--- setup.py
+++ setup.py
@@ -3,28 +3,44 @@
 
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+# Read requirements directly instead of from file
+requirements = [
+    "PyQt6>=6.5.0",
+    "matplotlib>=3.7.0",
+    "numpy>=1.24.0",
+    "pandas>=2.0.0",
+    "scipy>=1.10.0",
+    "openpyxl>=3.1.0",
+]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="corrosim",
     version="1.0.0",
+    author="Khaled Oukil",
+    author_email="oukil.khaled@gmail.com",
     description="Professional Corrosion Analysis Platform",
-    author="Your Name",
-    author_email="your.email@example.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/khadev/CorroSim",
     packages=find_packages(),
-    install_requires=requirements,
-    entry_points={
-        'console_scripts': [
-            'corrosim=corrosim.main:main',
-        ],
-    },
-    python_requires='>=3.8',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Chemistry",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "corrosim=corrosim.main:main",
+        ],
+    },
 )
